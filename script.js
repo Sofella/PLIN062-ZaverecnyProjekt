d76bb14f651b58afd8d8152c5feb5a6d54f7573a
Sofella <58860633+Sofella@users.noreply.github.com>
2023-05-31 15:51:15 +0200

    form and modal stuff, help

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Hello!")
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the form element
+var form = document.getElementById("contactForm");
+
+// Get the button that opens the modal
+var btn = document.getElementById("submitBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function(event) {
+    event.preventDefault();
+    // Get the form values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    
+    // Update the modal content with the submitted values
+    document.getElementById("modalName").textContent = "Name: " + name;
+    document.getElementById("modalEmail").textContent = "Email: " + email;
+    document.getElementById("modalMessage").textContent = "Message: " + message;
+  
+    modal.style.display = "block";
+
+    form.reset();
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
